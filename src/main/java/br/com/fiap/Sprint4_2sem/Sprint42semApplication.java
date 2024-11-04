af08c476a4b0948dbe72447823dbc53a89d58a9d
julia.andrade <julia.andrade@dpsp.com.br>
2024-11-04 14:42:38 -0300

    Commit Initial

---
+++ src/main/java/br/com/fiap/Sprint4_2sem/Sprint42semApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Sprint4_2sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint42semApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint42semApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/Sprint4_2sem/Sprint42semApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Sprint4_2sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint42semApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
